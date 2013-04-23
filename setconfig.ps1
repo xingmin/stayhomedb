407c6310193ac9a78e002828acf09e842999edaf
xingmin liu <xingminliu@qq.com>
2013-04-23 15:33:14 +0800

    test

---
+++ setconfig.ps1
@@ -0,0 +1,8 @@
+﻿$key = $args[0]
+$value = $args[1]
+Write-Host $key $value "$key=[`" .]*"
+#生成本机的唯一标识码
+$uuid = [System.Guid]::NewGuid().toString();
+get-content -path .\config.ps1 | %{$_ -replace "$key=.*","$key=$value"} > ".\$uuid.tmp"
+Remove-Item -Path .\config.ps1
+Move-Item -Path ".\$uuid.tmp" -Destination ".\config.ps1"
---
+++ setup.ps1
@@ -0,0 +1,5 @@
+﻿#生成本机的唯一标识码
+$uuid = [System.Guid]::NewGuid().toString();
+get-content -path .\config.ps1 | %{$_ -replace '\$MACHINE_UUID=[" .]*',"`$MACHINE_UUID=`"$uuid`""} > .\$uuid.tmp
+Remove-Item -Path .\config.ps1
+Move-Item -Path ".\$uuid.tmp" -Destination ".\config.ps1"
